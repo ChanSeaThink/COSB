bd05446d4e709b1f744b9f5856abab92d57d700c
Robin <42406671@qq.com>
2015-03-21 15:54:35 +0800

    验证码
    完成验证码的生成以及，看不清重新刷新验证码。

---
+++ COSB/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^hello$','login.views.hello'),
+    url(r'^login$','login.views.login'),
+    url(r'^getCAPTCHA','login.views.getCAPTCHA'),
+)
---
+++ login/views.py
@@ -0,0 +1,70 @@
+# -*- coding: utf8 -*-
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+import Image, ImageDraw, ImageFont, ImageFilter, random
+from hashlib import sha1
+from datetime import datetime
+import cStringIO
+
+# Create your views here.
+def hello(request):
+    return HttpResponse('Hello World')
+
+def login(request):
+    return render_to_response('login.html')
+
+def getCAPTCHA(request):
+    sh = sha1()
+    sh_src = sh.update(str(datetime.now()))
+    sh_src = sh.hexdigest()
+    code = sh_src[0:4]
+    #string = {'number':'12345679','litter':'ACEFGHKMNPRTUVWXY'}
+    background = (random.randrange (230,255),random.randrange(230,255),random.randrange(230,255))
+    line_color = ['red', 'blue', 'yellow', 'green', 'brown']
+    img_width = 120
+    img_height = 42 
+    font_color = ['black','red', 'blue', 'green', 'brown']
+    point_color = ['red', 'blue', 'yellow', 'green', 'brown']
+    font_size = 25
+    font = ImageFont.truetype('msyh.ttf',font_size)
+    #新建画布
+    im = Image.new('RGB',(img_width,img_height),background)
+    draw = ImageDraw.Draw(im)
+    #code = random.sample(string['litter'],4)
+    #code = u'调和社会'
+    #新建画笔
+    draw = ImageDraw.Draw(im)
+    #干扰点(70表示30%的概率)
+    for w in range(img_width):
+        for h in range(img_height):
+            tmp = random.randrange(1,100)
+            if tmp > 90:
+                draw.point((w, h), fill=(random.choice(point_color)))
+    #画干扰线
+    for i in range(random.randrange(5,8)):
+        xy = (random.randrange(0,img_width),random.randrange(0,img_height),
+              random.randrange(0,img_width),random.randrange(0,img_height))
+        draw.line(xy,fill=(random.choice(line_color)),width=1)
+    #写入验证码文字
+    x = 10
+    for i in code:
+        y = random.randrange(0,7)
+        draw.text((x,y), i, font=font, fill=random.choice(font_color))
+        x += 20
+    del x
+    del draw
+    params = [1 - float(random.randint(1, 2)) / 100,
+                  0,
+                  0,
+                  0,
+                  1 - float(random.randint(1, 10)) / 100,
+                  float(random.randint(1, 2)) / 500,
+                  0.001,
+                  float(random.randint(1, 2)) / 500
+                  ]
+    im = im.transform((120,42), Image.PERSPECTIVE, params) # 创建扭曲
+    buf = cStringIO.StringIO()
+    im.save(buf, 'gif')
+    #buf.seek(0)
+    return HttpResponse(buf.getvalue(), 'image/gif')
